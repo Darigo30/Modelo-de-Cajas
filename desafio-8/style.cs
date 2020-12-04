df538ca21a1bd65dfeed9b4e9262e4f2f9e4b35a
Dariana Gómez <darigomez30@gmail.com>
2020-12-03 23:13:48 -0300

    Se traspasa todo a scss

---
+++ desafio-8/style.cs
@@ -0,0 +1,32 @@
+html, body {
+  margin: 0;
+  padding: 0;
+}
+
+menu {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+}
+menu li {
+  background: olive;
+  box-sizing: border-box;
+  float: left;
+  width: 33.3%;
+}
+menu li a {
+  color: #FFF;
+  display: block;
+  padding: 10px;
+  text-decoration: none;
+}
+menu li a:hover {
+  background: #000;
+}
+
+p {
+  clear: both;
+  padding: 20px;
+}
+
+/*# sourceMappingURL=style.cs.map */
